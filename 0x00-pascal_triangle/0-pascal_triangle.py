0392ff81998bd1a7acce0847a2d52ee1d7dbaf80
n-elmasry <almassrynada411@gmail.com>
2024-07-28 16:17:26 +0500

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """ Pascal's Triangle """
-from math import factorial
+
+
+def factorial(n):
+    """Calculates the factorial of n (n!)."""
+    if n == 0 or n == 1:
+        return 1
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
 
 
 def pascal_triangle(n):
-    """  returns a list of lists of integers
+    """returns a list of lists of integers
     representing the Pascals triangle of n"""
     if n <= 0:
         return []
