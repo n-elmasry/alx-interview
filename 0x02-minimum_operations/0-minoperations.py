066b6e4a2222f9810fc11fd64687e304db06e5b3
n-elmasry <almassrynada411@gmail.com>
2024-08-11 19:58:09 +0500

    task0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """minOperations"""
-import math
 
 
 def minOperations(n: int) -> int:
     """calculates the fewest number of operations needed
     to result in exactly n H characters in the file."""
-    minop = 0
+    opnum = 0
+    minop = 2
 
     if n <= 1:
         return 0
 
-    while n % 2 == 0:
-        n = n // 2
+    while n > 1:
+        while n % minop == 0:
+            opnum += minop
+            n //= minop
         minop += 1
-
-    for i in range(3, int(math.sqrt(n))+1, 2):
-        while n % i == 0:
-            n = n // i
-            minop += i
-    if n > 2:
-        minop += n
-
-    return minop
+    return opnum
