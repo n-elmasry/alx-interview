fe12c645cf95b98f84fbd30e4f726ef349f1ced8
n-elmasry <almassrynada411@gmail.com>
2024-09-01 17:15:51 +0500

    task 0

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""nqueens"""
+import sys
+
+
+def nqueens():
+    """nqueens"""
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
+
+    if n < 4:
+        print('N must be at least 4')
+        sys.exit(1)
+
+    column = set()
+    positive_diagonal = set()
+    negative_diagonal = set()
+
+    result = []
+
+    def backtrack(r, solution):
+        """backtrack"""
+        if r == n:
+            print(solution)
+            return
+
+        for c in range(n):
+            if (c in column
+                or (r + c) in positive_diagonal
+                    or (r - c) in negative_diagonal):
+                continue
+
+            column.add(c)
+            positive_diagonal.add(r + c)
+            negative_diagonal.add(r - c)
+            solution.append([r, c])
+
+            backtrack(r + 1, solution)
+
+            column.remove(c)
+            positive_diagonal.remove(r + c)
+            negative_diagonal.remove(r - c)
+            solution.pop()
+
+    backtrack(0, [])
+
+
+if __name__ == '__main__':
+    nqueens()
