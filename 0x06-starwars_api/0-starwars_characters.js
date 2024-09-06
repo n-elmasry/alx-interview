6c6303162d5f8058bd3a20f158ad1e7949f89117
n-elmasry <almassrynada411@gmail.com>
2024-09-06 21:17:45 +0500

    fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,30 +6,30 @@ const url = 'https://swapi-api.alx-tools.com/api';
 const movieId = process.argv[2];
 
 request(`${url}/films/${movieId}/`, (error, response, body) => {
-    if (error) {
-        console.error('Error fetching movie:', error);
-        return;
-    }
-    const filmData = JSON.parse(body);
-    const characterUrls = filmData.characters;
+  if (error) {
+    console.error('Error fetching movie:', error);
+    return;
+  }
+  const filmData = JSON.parse(body);
+  const characterUrls = filmData.characters;
 
-    const characterRequests = characterUrls.map(url => {
-        return new Promise((resolve, reject) => {
-            request(url, (charError, charResponse, charBody) => {
-                if (charError) {
-                    reject(charError);
-                } else {
-                    resolve(JSON.parse(charBody));
-                }
-            });
-        });
+  const characterRequests = characterUrls.map(url => {
+    return new Promise((resolve, reject) => {
+      request(url, (charError, charResponse, charBody) => {
+        if (charError) {
+          reject(charError);
+        } else {
+          resolve(JSON.parse(charBody));
+        }
+      });
     });
+  });
 
-    Promise.all(characterRequests)
-        .then(characters => {
-            characters.forEach(character => {
-                console.log(character.name);
-            });
-        })
-        .catch(error => console.error('Error fetching character:', error));
+  Promise.all(characterRequests)
+    .then(characters => {
+      characters.forEach(character => {
+        console.log(character.name);
+      });
+    })
+    .catch(error => console.error('Error fetching character:', error));
 });
