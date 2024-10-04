075a83b9cd5de3ed746d422216f5360734df48bc
n-elmasry <almassrynada411@gmail.com>
2024-10-04 07:52:26 +0500

    task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""makeChange"""
+
+
+def makeChange(coins, total):
+    """determine the fewest number of coins
+    needed to meet a given amount total"""
+    if total == 0:
+        return 0
+    if total < 0:
+        return -1
+    dp = [total + 1] * (total + 1)
+
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total+1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != total + 1 else -1
